f4fcc57524d27356d06073aab19a2930747e406a
Amol <amol.nagotkar@gmail.com>
2020-01-11 19:16:11 +0530

    update

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express=require('express');
 const app=express();
 const path=require('path')
 
-app.use(express.static(__dirname+'/dist'))
+app.use(express.static('./dist/Angular-7-Demo-App-master'))
 const port=process.env.PORT || 8080;
-app.listen(port,()=>{
-    console.log(`app listening on port ${port}`)
-});
-app.get("/",function (req,res) {
-    res.sendFile(path.join(__dirname+"/dist/index.html"))
+
+app.get("/*",function (req,res) {
+    res.sendFile(path.join(__dirname+"/dist/Angular-7-Demo-App-master/index.html"))
 })
+app.listen(port,()=>{
+  console.log(`app listening on port ${port}`)
+});
