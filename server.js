4b8df756d4b2d34431e4526fcf475141b1da6868
Amol <amol.nagotkar@gmail.com>
2020-01-11 19:24:22 +0530

    update

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express=require('express');
 const app=express();
 const path=require('path')
 
-app.use(express.static('./dist/Angular-7-Demo-App-master'))
+app.use(express.static(__dirname+'/dist'))
 const port=process.env.PORT || 8080;
 
 app.get("/*",function (req,res) {
-    res.sendFile(path.join(__dirname+"/dist/Angular-7-Demo-App-master/index.html"))
+    res.sendFile(path.join(__dirname+"/dist/index.html"))
 })
 app.listen(port,()=>{
   console.log(`app listening on port ${port}`)
