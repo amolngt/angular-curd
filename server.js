4865bf5964c48a1be6c51a8b62a56e4f46c73fdd
Amol <amol.nagotkar@gmail.com>
2020-01-11 19:49:11 +0530

    updated

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path=require('path')
 
 app.use(express.static(__dirname+'/dist/ang7'))
 const port=process.env.PORT || 8080;
-
-app.get("/*",function (req,res) {
-    res.sendFile(path.join(__dirname+"/dist/ang7/index.html"))
-})
 app.listen(port,()=>{
   console.log(`app listening on port ${port}`)
 });
+app.get("/*",function (req,res) {
+    res.sendFile(path.join(__dirname+"/dist/ang7/index.html"))
+})
+
